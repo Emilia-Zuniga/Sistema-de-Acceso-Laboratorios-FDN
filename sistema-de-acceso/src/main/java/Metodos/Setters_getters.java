7c133daf9601775ecc13319a2f5f1368c0dc8fa2
Emilia Zuñiga Losada <82168337+Emilia-Zuniga@users.noreply.github.com>
2023-10-30 22:55:05 -0600

    Add files via upload

---
+++ sistema-de-acceso/src/main/java/Metodos/Setters_getters.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Metodos;
+
+public class Setters_getters {
+    
+    private static String contra;
+    private static String usuario;
+
+    public static String getContra() {
+        return contra;
+    }
+
+    public static String getUsuario() {
+        return usuario;
+    }
+
+    public static void setContra(String contra) {
+        Setters_getters.contra = contra;
+    }
+
+    public static void setUsuario(String usuario) {
+        Setters_getters.usuario = usuario;
+    }
+
+    
+}
